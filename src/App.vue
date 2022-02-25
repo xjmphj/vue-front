fa7f50c1c33d19ba710f1bb5d9ee2fe518c23b06
xiejunmei <xiejunme@gomeplus.com>
2022-02-25 22:28:43 +0800

    🎉 init(all): all 项目初始化

---
+++ src/App.vue
@@ -0,0 +1,46 @@
+<template>
+  <ul id="app">
+    <li v-for="item in users" :key="item.id">
+      {{item.name}}
+    </li>
+  </ul>
+</template>
+<script>
+import axios from 'axios';
+export default {
+  name:'app',
+  data(){
+    return {
+      users:[]
+    }
+  },
+  mounted(){
+    axios.get('http://localhost:3000/api/users').then((res)=>{
+      this.users = res.data
+    })
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+#nav {
+  padding: 30px;
+}
+
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+#nav a.router-link-exact-active {
+  color: #42b983;
+}
+</style>
